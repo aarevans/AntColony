86935eccf46372ecac5af9835d893ff06472324c
Aaron Evans <aaron_evans@outlook.com>
2022-05-25 18:52:18 +0100

    Updated graph creation

--- Program.py
+++ Program.py
@@ -7,25 +7,30 @@ G=nx.Graph()
 G.add_nodes_from(['A','B','C','D','E'])
 
 # Use add_edges_from to add pairwise relationships
-G.add_edge('B','C',weight=0.5)
-G.add_edge('A','C',weight=0.5)
-G.add_edge('B','D',weight=0.5)
-G.add_edge('D','A',weight=0.5)
-G.add_edge('D','E',weight=0.5)
-G.add_edge('B','E',weight=0.5)
+G.add_edge('B','C',weight=1)
+G.add_edge('A','C',weight=1)
+G.add_edge('B','D',weight=1)
+G.add_edge('D','A',weight=1)
+G.add_edge('D','E',weight=1)
+G.add_edge('B','E',weight=1)
 
-print(G.nodes())
-print(G.edges())
+#print(G.nodes())
+#print(G.edges())
 
+#Creates the graph layout using all nodes and edges and stores in pos variable
 pos=nx.spring_layout(G)
 
 #pos=nx.get_node_attributes(G,'pos')
 
-nx.draw(G,pos)
+#creates visual representation of the graph (including labelling nodes)
+nx.draw(G,pos, with_labels=True)
 
+#Stores the weight values for each edge in a labales variable
 labels = nx.get_edge_attributes(G,'weight')
 
-nx.draw_networkx_labels(G,pos,edge_labels=labels,with_labels=True)
+#Adds edge labels for the weights to the graph
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
 
+#Saves the created graph in a png file.
 plt.savefig("graph.png")
 
